8a7db6b5230890d8bf95fa37af49e32bd93e6512
pkzvh <ppaul.bellamy@gmail.com>
2024-03-20 17:21:37 +0900

    style: fix typo in imports

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -2,14 +2,14 @@ import ClassificationSelect from './ClassificationSelect.component';
 import CountrySelect from './CountrySelect.component';
 import React from 'react';
 import SubclassificationSelect from './SubclassificationSelect.component';
-import useDistinctDomainParts from '../../hooks/useDomainParts';
+import useDomainParts from '../../hooks/useDomainParts';
 
 interface Props {
   domains: string[]
 }
 
 const DomainFilter: React.FC<Props> = ({ domains }) => {
-  const { countries, classifications, subClassifications } = useDistinctDomainParts(domains);
+  const { countries, classifications, subClassifications } = useDomainParts(domains);
 
   return (<>
     <CountrySelect countries={countries} />
