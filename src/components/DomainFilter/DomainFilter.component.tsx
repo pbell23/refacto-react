ad049cc02e0f219522b0c07662bcc655d4322fd8
pkzvh <ppaul.bellamy@gmail.com>
2024-03-20 15:13:59 +0900

    refactor: extract domain filtering logic to a lib

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { extractDomainParts } from '../../lib/domainFilter';
 import { threadId } from 'worker_threads';
 
 interface State {
@@ -14,40 +15,20 @@ interface Props {
 class DomainFilter extends React.Component<Props, State> {
   componentDidMount() {
     const { domains } = this.props
-    this.state = {
-      countries: [],
-      classifications: [],
-      subClassifications: []
-    }
 
     const s: any = {};
 
-    for(let i = 0; i < domains.length; i++) {
-      if (this.state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        this.state.countries.push(domains[i].substring(0,2))
-      }
-      this.state.classifications.push(domains[i].substring(3,5));
-      let flag = false;
-      for(let j = 0; j < this.state.subClassifications.length; j++) {
-        if (this.state.subClassifications[j] == domains[i].substring(6)) {
-          flag = true
-          break;
-        }
-      }
-      if (!flag) {
-        this.state.subClassifications.push(domains[i].substring(6));
-      }
-    }
+    const domainParts = extractDomainParts(domains)
 
     this.setState({
-      ...this.state,
-      classifications: this.state.classifications.filter((e, i, l) => l.indexOf(e) === i),
+      ...domainParts,
+      classifications: domainParts.classifications.filter((e, i, l) => l.indexOf(e) === i),
     })
     this.forceUpdate()
   }
 
   render() {
-    const {countries, classifications, subClassifications} = this.state || {
+    const { countries, classifications, subClassifications } = this.state || {
       countries: [],
       classifications: [],
       subClassifications: []
---
+++ src/lib/domainFilter.ts
@@ -0,0 +1,30 @@
+interface IDomainParts {
+    countries: string[],
+    classifications: string[],
+    subClassifications: string[]
+}
+
+export const extractDomainParts = (domains: string[]): IDomainParts => {
+    const countries: string[] = []
+    const classifications: string[] = []
+    const subClassifications: string[] = []
+
+    for (let i = 0; i < domains.length; i++) {
+        if (countries.indexOf(domains[i].substring(0, 2)) <= 0) {
+            countries.push(domains[i].substring(0, 2))
+        }
+        classifications.push(domains[i].substring(3, 5));
+        let flag = false;
+        for (let j = 0; j < subClassifications.length; j++) {
+            if (subClassifications[j] == domains[i].substring(6)) {
+                flag = true
+                break;
+            }
+        }
+        if (!flag) {
+            subClassifications.push(domains[i].substring(6));
+        }
+    }
+
+    return { countries, classifications, subClassifications }
+}
