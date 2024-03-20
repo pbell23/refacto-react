e2ebea16352bb8b8e41b40840e71080deb7867d7
pkzvh <ppaul.bellamy@gmail.com>
2024-03-20 17:52:47 +0900

    refactor: add all the filtering logic inside useDomainParts hook

--- src/hooks/useDomainParts.ts
+++ src/hooks/useDomainParts.ts
@@ -1,5 +1,37 @@
-import { extractDomainParts } from '../lib/domainFilter';
 import { useMemo } from 'react';
+
+interface IDomainParts {
+    countries: string[],
+    classifications: string[],
+    subClassifications: string[]
+}
+
+const extractDomainParts = (domains: string[]): IDomainParts => {
+    const countries: string[] = []
+    let classifications: string[] = []
+    const subClassifications: string[] = []
+
+    for (let i = 0; i < domains.length; i++) {
+        if (countries.indexOf(domains[i].substring(0, 2)) <= 0) {
+            countries.push(domains[i].substring(0, 2))
+        }
+        classifications.push(domains[i].substring(3, 5));
+        let flag = false;
+        for (let j = 0; j < subClassifications.length; j++) {
+            if (subClassifications[j] === domains[i].substring(6)) {
+                flag = true
+                break;
+            }
+        }
+        if (!flag) {
+            subClassifications.push(domains[i].substring(6));
+        }
+    }
+
+    classifications = classifications.filter((e, i, l) => l.indexOf(e) === i)
+
+    return { countries, classifications, subClassifications }
+}
 
 const useDomainParts = (domains: string[]) => {
     const domainParts = useMemo(() => extractDomainParts(domains), [domains]);
