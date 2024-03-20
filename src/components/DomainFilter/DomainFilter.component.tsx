be08806a6b993e78bc512d3852e034aba617edc7
pkzvh <ppaul.bellamy@gmail.com>
2024-03-20 15:32:47 +0900

    refactor: update from class to functional component

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { extractDomainParts } from '../../lib/domainFilter';
 
-interface State {
+interface IDomainParts {
   countries: string[],
   classifications: string[],
   subClassifications: string[]
@@ -11,44 +12,38 @@ interface Props {
   domains: string[]
 }
 
-class DomainFilter extends React.Component<Props, State> {
-  componentDidMount() {
-    const { domains } = this.props
+const initialDomainParts: IDomainParts = {
+  countries: [],
+  classifications: [],
+  subClassifications: []
+}
 
-    const domainParts = extractDomainParts(domains)
+const DomainFilter: React.FC<Props> = ({ domains }) => {
+  const [domainParts, setDomainParts] = useState(initialDomainParts);
+  const { countries, classifications, subClassifications } = domainParts;
 
-    this.setState({
-      ...domainParts,
-      classifications: domainParts.classifications.filter((e, i, l) => l.indexOf(e) === i),
-    })
-    this.forceUpdate()
-  }
+  useEffect(() => {
+    const updatedDomainParts = extractDomainParts(domains);
+    setDomainParts({ ...updatedDomainParts, classifications: updatedDomainParts.classifications.filter((e, i, l) => l.indexOf(e) === i), })
+  }, [domains])
 
-  render() {
-    const { countries, classifications, subClassifications } = this.state || {
-      countries: [],
-      classifications: [],
-      subClassifications: []
-    };
-
-    return (<>
-      <select name="countries" multiple>
-        {countries.map(country => (
-          <option value={country} key={country}>{country}</option>
-        ))}
-      </select>
-      <select name="classifications" multiple>
-        {classifications.map(classification => (
-          <option value={classification} key={classification}>{classification}</option>
-        ))}
-      </select>
-      <select name="subClassifications" multiple>
-        {subClassifications.map(subClassification => (
-          <option value={subClassification} key={subClassification}>{subClassification}</option>
-        ))}
-      </select>
-    </>)
-  }
+  return (<>
+    <select name="countries" multiple>
+      {countries.map(country => (
+        <option value={country} key={country}>{country}</option>
+      ))}
+    </select>
+    <select name="classifications" multiple>
+      {classifications.map(classification => (
+        <option value={classification} key={classification}>{classification}</option>
+      ))}
+    </select>
+    <select name="subClassifications" multiple>
+      {subClassifications.map(subClassification => (
+        <option value={subClassification} key={subClassification}>{subClassification}</option>
+      ))}
+    </select>
+  </>)
 }
 
 export default DomainFilter
