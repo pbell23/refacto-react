2c33ef427bed54939d2196f3eb893357c8e07e27
pkzvh <ppaul.bellamy@gmail.com>
2024-03-20 17:02:51 +0900

    refactor: use a custom hook instead of a simple function

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,34 +1,15 @@
-import React, { useEffect, useState } from 'react';
-
 import ClassificationSelect from './ClassificationSelect.component';
 import CountrySelect from './CountrySelect.component';
+import React from 'react';
 import SubclassificationSelect from './SubclassificationSelect.component';
-import { extractDomainParts } from '../../lib/domainFilter';
-
-interface IDomainParts {
-  countries: string[],
-  classifications: string[],
-  subClassifications: string[]
-}
+import useDistinctDomainParts from '../../hooks/useDomainParts';
 
 interface Props {
   domains: string[]
 }
 
-const initialDomainParts: IDomainParts = {
-  countries: [],
-  classifications: [],
-  subClassifications: []
-}
-
 const DomainFilter: React.FC<Props> = ({ domains }) => {
-  const [domainParts, setDomainParts] = useState(initialDomainParts);
-  const { countries, classifications, subClassifications } = domainParts;
-
-  useEffect(() => {
-    const updatedDomainParts = extractDomainParts(domains);
-    setDomainParts({ ...updatedDomainParts, classifications: updatedDomainParts.classifications.filter((e, i, l) => l.indexOf(e) === i), })
-  }, [domains])
+  const { countries, classifications, subClassifications } = useDistinctDomainParts(domains);
 
   return (<>
     <CountrySelect countries={countries} />
